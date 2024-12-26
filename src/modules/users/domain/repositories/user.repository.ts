32f819f186feb0ef3298ef3b0fadb616597afb31
sisunmerco <creativesbroken@gmail.com>
2024-12-26 17:30:55 -0500

    update

--- src/modules/users/domain/repositories/user.repository.ts
+++ src/modules/users/domain/repositories/user.repository.ts
@@ -3,6 +3,7 @@ import { UserEntity } from "../entities/user.entity";
 
 export abstract class UserRepository {
     abstract findByEmail(email: string): Promise<UserEntity>;
+    abstract findById(id: string): Promise<UserEntity>;
     abstract findAll(): Promise<UserEntity[]>;
     abstract create(user: CreateUserDto): Promise<UserEntity>;
 }
--- src/modules/users/infrastructure/mongodb.repositoryImpl.ts
+++ src/modules/users/infrastructure/mongodb.repositoryImpl.ts
@@ -4,6 +4,7 @@ import { UserRepository } from "../domain/repositories/user.repository";
 import { User } from "./databases/schemas/user.schema";
 import { UserEntity } from "../domain/entities/user.entity";
 import { CreateUserDto } from "../presentation/dtos/create-user.http.dto";
+import { hash } from 'bcrypt'
 
 export class MongoDBRespositoryImpl implements UserRepository {
 
@@ -11,8 +12,14 @@ export class MongoDBRespositoryImpl implements UserRepository {
     private readonly userModel: Model<User>) {
     }
 
+    async findById(id: string): Promise<UserEntity> {
+        return await this.userModel.findById(id).exec()
+    }
+
     async create(user: CreateUserDto): Promise<UserEntity> {
-        const newUser = new this.userModel(user)
+        const newPassword = await hash(user.password, 10)
+        const userAdd = { ...user, password: newPassword }
+        const newUser = new this.userModel(userAdd)
         return await newUser.save()
     }
 
