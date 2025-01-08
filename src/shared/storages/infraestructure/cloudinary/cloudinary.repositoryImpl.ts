3ae8bde1d68f77460a8935ddcfdcb67c8aa6bb7e
sisunmerco <creativesbroken@gmail.com>
2025-01-07 22:10:06 -0500

    Update cloudinary.repositoryImpl.ts

--- src/shared/storages/infraestructure/cloudinary/cloudinary.repositoryImpl.ts
+++ src/shared/storages/infraestructure/cloudinary/cloudinary.repositoryImpl.ts
@@ -41,28 +41,23 @@ export class CloudinaryRepositoryImpl implements UploadStorageRepository {
     }
 
     async update(id: string, file: Express.Multer.File): Promise<UploadApiResponse | UploadApiErrorResponse> {
-       
-            // Validar parámetros de entrada
-            if (!id) throw new HttpException('El ID de la imagen es obligatorio.', 401);
-            if (!file) throw new HttpException('El archivo es obligatorio.', 401);
 
-            // Eliminar la imagen existente
-            const deleteResult = await cloudinary.uploader.destroy(id);
-            if (deleteResult.result !== 'ok') {
-                throw new HttpException('No se pudo eliminar la imagen con ID: ${id}', 404)
-            }
+        if (!id) throw new HttpException('El ID de la imagen es obligatorio.', 401);
+        if (!file) throw new HttpException('El archivo es obligatorio.', 401);
 
-            // Subir la nueva imagen
-            const uploadResult: UploadApiResponse | UploadApiErrorResponse = await this.upload(file);
+        const deleteResult = await cloudinary.uploader.destroy(id);
+        if (deleteResult.result !== 'ok') {
+            throw new HttpException('No se pudo eliminar la imagen con ID: ${id}', 404)
+        }
 
-            // Validar la respuesta de la subida
-            if ('error' in uploadResult) {
-                throw new HttpException(
-                    `Error al subir la imagen: ${uploadResult.error.message}`
-                    , 500);
-            }
+        const uploadResult: UploadApiResponse | UploadApiErrorResponse = await this.upload(file);
 
-            // Retornar la respuesta exitosa
-            return uploadResult;
+        if ('error' in uploadResult) {
+            throw new HttpException(
+                `Error al subir la imagen: ${uploadResult.error.message}`
+                , 500);
+        }
+
+        return uploadResult;
     }
 }
